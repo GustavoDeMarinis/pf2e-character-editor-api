9ba265e6a9ca228b782ad6e5c771ac26ab872f54
gustavodm <gustadema@gmail.com>
2025-03-28 01:43:24 -0300

    added validations to config.ts to check env variables are not missing

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 const env = dotenv.config();
 
+if (env.error) {
+  throw new Error("No .env file found");
+}
+
+if (!process.env.JWT_SECRET_KEY) {
+  throw new Error("No JWT_SECRET_KEY found in .env file");
+}
+
 export const config = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ?? "",
   JWT_EXPIRATION_PERIOD: "1d",
-  LOCAL_SALT_ROUNDS: 10,
+  LOCAL_SALT_ROUNDS: process.env.ENV === "prod" ? 14 : 10,
   ENV: process.env.ENV ?? "local",
   USER_DEFAULT_PASSWORD: process.env.USER_DEFAULT_PASSWORD ?? "",
 };
